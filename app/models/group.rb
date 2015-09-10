9248f47e8003cd0cc4a6e0e09cfc429e5fe7553e
willyhu <w.illy741026@gmail.com>
2015-09-10 05:02:51 +0000

    XXX

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,10 @@ class Group < ActiveRecord::Base
     # validates :title, presence: true
     # validates :title, presence: flase
     has_many :posts
+    
+    belongs_to :users, class_name: "User", foreign_key: :user_id
+    
+    # def editable_by?(user)
+    #     user && user == :user
+    # end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,10 @@
 class Post < ActiveRecord::Base
     validates :content, presence: {message: '此欄位不可空白'}
     belongs_to :group
+    
+    belongs_to :author, class_name: "User", foreign_key: :user_id
+    
+    # def editable_by?(user)
+    #     user && user == author
+    # end
 end
