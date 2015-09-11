c03a9f3d13b10e4c2c893ccc85f95934932f3836
willyhu <w.illy741026@gmail.com>
2015-09-11 14:49:42 +0000

    test data&task

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+puts "Running..."
+# create_account = User.create([email: 'example@gmail.com', password: '12345678', password_confirmation: '12345678', name: '測試用帳號'])
+create_groups = for i in 1..20 do
+    Group.create!([title: "Group no.#{i}", description: "測試資料第 #{i} 個討論版", user_id: "1"])
+    for k in 1..30 do
+        Post.create!([group_id: "#{i}",content: "測試資料第 #{k} 個留言", user_id: "1"])
+    end
+end
+puts "Finish!!!"
---
+++ lib/tasks/dev.rake
@@ -0,0 +1,4 @@
+namespace :dev do
+  desc "Rebuild system"
+  task :build => ["tmp:clear", "log:clear", "db:drop", "db:create", "db:migrate"]
+end
