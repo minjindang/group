fdcaf8330decdc25f614de15057e4a6ddad9e0a3
willyhu <w.illy741026@gmail.com>
2015-09-11 09:13:35 +0000

    Refactor Helper

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -50,7 +50,7 @@ class PostsController < ApplicationController
     end
     
     def member_required
-        return if !current_user.is_member_of?(@group)
+        return if current_user.is_member_of?(@group)
         flash[:warning] = "你不是這個討論版的成員，不能發文喔！"
         redirect_to group_path(@group)
     end
